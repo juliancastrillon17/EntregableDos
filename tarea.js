8e693988f433684ee03f3efcf24ae77b527a767d
Julian Castrillon <julian.castrillon234@pascualbravo.edu.co>
2022-04-21 21:23:25 -0500

    tarea.js modificada con cada prueba pasada

--- tarea.js
+++ tarea.js
@@ -1,22 +1,35 @@
-const suma = (a,b) =>{
+const suma = (x,y) =>{
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código
+    return x + y;
     
 }
+
 
 const sonIguales = (x, y)=>{
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
-    
+    if(x == y){
+      return true; 
+    }else{
+      return false; 
+    }
 }
+
 
 function tienenMismaLongitud(str1, str2) {
     // Devuelve "true" si las dos strings tienen la misma longitud
     // De lo contrario, devuelve "false"
     // Tu código:
+    if(str1.length == str2.length){
+      return true; 
+    }else{
+      return false; 
+    }
   }
+
 
   function esPositivo(numero) {
     //La función va a recibir un entero. Devuelve como resultado una cadena de texto que indica si el número es positivo o negativo. 
@@ -24,8 +37,15 @@ function tienenMismaLongitud(str1, str2) {
     //Si el número es negativo, devolver ---> "Es negativo"
     //Si el número es 0, devuelve false
     // Tu código:
-    
+    if(numero > 0){
+      return "Es positivo";
+    }else if(numero < 0){
+      return "Es negativo";
+    }else if(numero == 0){
+      return false; 
+    }
   }
+
 
   const colors = (color)=>{
     //La función recibe un color. Devolver el string correspondiente:
@@ -35,48 +55,98 @@ function tienenMismaLongitud(str1, str2) {
     //En caso que el color recibido sea "orange", devuelve --> "This is orange"
     //Caso default: devuelve --> "Color not found"
     //Usar el statement Switch.
-    
+    switch(color){
+      case "blue": 
+        return "This is blue";
+        break;
+      case "red": 
+        return "This is red";
+        break;
+      case "green": 
+        return "This is green";
+        break;
+      case "orange": 
+        return "This is orange";
+        break; 
+      default: 
+        return "Color not found";
+    }
   }
+
+
 
   const fizzBuzz = (numero)  => {
     // Si "numero" es divisible entre 3, devuelve "fizz"
     // Si "numero" es divisible entre 5, devuelve "buzz"
     // Si "numero" es divisible entre 3 y 5 (ambos), devuelve "fizzbuzz"
     // De lo contrario, devuelve el numero
-    
+    if(numero % 3 == 0 && numero % 5 == 0){
+      return "fizzbuzz";
+    }else if(numero%3 == 0 && numero%5 != 0){
+      return "fizz";
+    }else if(numero%3 != 0 && numero%5 == 0){
+      return "buzz";
+    }else{
+      return numero;
+    }
   }
+
+
 
   const devolverPrimerElemento = (array) => {
     // Devuelve el primer elemento de un array
     // Tu código:
-   
+    return array[0];
   }
+
+
 
   const agregarItemAlFinalDelArray = (array, elemento) =>{
     // Añade el "elemento" al final del array
     // y devuelve el array
     // Tu código:
+    array.push(elemento);
+    return array; 
   }
+
+
 
   const nuevoUsuario = (nombre, email, password) => {
     // Crea un nuevo objeto con las propiedades coincidiendo con los argumentos que se pasan a la función
     // Devuelve el objeto
     // Tu código:
+    let usuario = {
+      nombre: nombre,
+      email: email,
+      password: password
+    }; 
+
+    return usuario; 
   }
+
+  
 
   const verificarPassword = (usuario, password) => {
     // Comprueba si el "password" enviado coincide con la propiedad "password" del objeto "usuario"
     // Devuelve "true" si coinciden
     // De lo contrario, devuelve "false"
     // // Tu código:
+    if(password == usuario.password){
+      return true; 
+    }else{
+      return false; 
+    }
   }
+
 
   const actualizarPassword =(usuario, nuevaPassword) => {
     // Reemplaza la contraseña existente en el objeto "usuario" con el valor de "nuevaPassword"
     // Devuelve el objeto
     // Tu código:
-    
+    usuario.password = nuevaPassword
+    return usuario;
   }
+
 
   const agregarAmigo = (usuario, nuevoAmigo) => {
     // "usuario" tiene una propiedad llamada "amigos" que es un array
@@ -86,8 +156,10 @@ function tienenMismaLongitud(str1, str2) {
     // var user = {
     //   amigos: ['Pedro', 'Sebastian']
     // }
-   
+    usuario.amigos.push(nuevoAmigo);
+    return usuario;
   }
+
 
   const pasarUsuarioAPremium = usuarios => {
     // "usuarios" es un array de objetos "usuario"
@@ -96,7 +168,13 @@ function tienenMismaLongitud(str1, str2) {
     // Devuelve el array de usuarios
     // Tu código:
     
+    for(let element in usuarios){
+      usuarios[element].esPremium = true;
+    }
+    
+    return usuarios;
   }
+
 
   const sumarLikesDeUsuario = usuario => {
     // "usuario" tiene una propiedad llamada "posts" que es un array
@@ -108,10 +186,19 @@ function tienenMismaLongitud(str1, str2) {
     // let usuario = {
     //   posts: [{
     //     likes: 4
-    //   }]
+    //   },]
     // };
 
+      let suma = 0; 
+
+      for(let element in usuario.posts){
+        suma += usuario.posts[element].likes;
+      }
+
+    return suma;
   }
+
+
 
   // ---------------------------------------------------------------------------//
   //Crea el constructor de la clase "Persona"
@@ -126,44 +213,85 @@ function tienenMismaLongitud(str1, str2) {
     //  }
 
 class Persona {
-    constructor(/*Escribir los argumentos que recibe el constructor*/) {
-      // Crea el constructor:
-
+    constructor(nombre,apellido,edad,domicilio) {
+        this.nombre = nombre;
+        this.apellido = apellido; 
+        this.edad = edad; 
+        this.domicilio = domicilio;
     }
-}
+
+    detalle(propiedadPersona){
+      //Objeto; 
+      propiedadPersona = {
+        nombre: this.nombre,
+        apellido: this.apellido,
+        edad: this.edad,
+        domicilio: this.domicilio
+      }
+
+      return propiedadPersona;
+    }
+} 
+
+
+
 
 const crearInstanciaPersona = (nombre, apellido, edad, dir)=>{
     //Con esta función vamos a crear una nueva persona a partir de nuestro constructor de persona (creado en el ejercicio anterior)
     //Recibirá los valores "Juan", "Perez", 22, "Saavedra 123" para sus respectivas propiedades
     //Devolver la nueva persona creada
-    
+    //tu código: 
+    let personaDos = new Persona(nombre , apellido, edad, dir);
+    return personaDos;
 }
+
+
 
 const sumarArray = (numeros, cb) =>{
     // Suma todos los números enteros (int/integers) de un array ("numeros")
     // Pasa el resultado a `cb`
     // No es necesario devolver nada
+    let suma = 0; 
+
+    for(let element in numeros){
+       suma += numeros[element];
+     }
+
+    return cb(suma);
 }
 
 const copiarEach = (array, cb) =>{
     // Itera sobre la matriz "array" y pasa los valores a cb uno por uno
     // Pista: Estarás invocando a `cb` varias veces (una por cada valor en el array)
     // no tienes que devolver nada
-    
+
+    for(let element in array)
+      cb(array[element]);
   }
 
-  const operacionMatematica =(n1, n2, cb) =>{
+
+  const operacionMatematica = (n1, n2, cb) =>{
     //Vamos a recibir una función que realiza una operación matemática junto con dos números.
     //Devolver la función pasándole como argumentos los números recibidos.
     //Tu código:
+    return cb(n1,n2);
   }
+
 
   function filter(array) {
     //Filtrar todos los elementos del array que comiencen con la letra "a".
     //Devolver un nuevo array con los elementos que cumplen la condición
     //Tu código:
-  } 
+    let nuevo = [];
 
+    array.filter(element=>{
+      if(element.charAt() == 'a'){
+          nuevo.push(element);
+      }
+    }); 
+
+    return nuevo; 
+  }
 
   /**
  * Por favor no borrar el código que hay de aquí en adelante.
